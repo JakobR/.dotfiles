089c38393c10a7cdc6b7817e43cfe5eeb8815d85
Jakob Rath <git@jakobrath.eu>
2012-12-12 16:29:52 +0100

    Extract rvm setup.

---
+++ custom/rvm.zsh
@@ -0,0 +1,6 @@
+
+# Load RVM into a shell session *as a function*
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# As described on https://rvm.io/integration/zsh/
+__rvm_project_rvmrc
