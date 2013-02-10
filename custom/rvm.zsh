6b3518519b618b0c8e6df458665929e0ba569d50
Jakob Rath <git@jakobrath.eu>
2013-02-10 21:03:22 +0100

    Add support for system-wide rvm.

--- custom/rvm.zsh
+++ custom/rvm.zsh
@@ -1,5 +1,6 @@
 
 # Load RVM into a shell session *as a function*
+[[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 # As described on https://rvm.io/integration/zsh/
