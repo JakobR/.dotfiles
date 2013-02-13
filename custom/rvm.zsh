db97a516d2f3706d4805bf493e2774bb9e074603
Jakob Rath <git@jakobrath.eu>
2013-02-13 11:12:18 +0100

    Better rvm loading.

--- custom/rvm.zsh
+++ custom/rvm.zsh
@@ -1,7 +1,16 @@
 
-# Load RVM into a shell session *as a function*
-[[ -s "/usr/local/rvm/scripts/rvm" ]] && source "/usr/local/rvm/scripts/rvm"
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+# Possible rvm install paths
+jr_rvm_paths=("$HOME/.rvm/scripts/rvm" "/usr/local/rvm/scripts/rvm")
 
-# As described on https://rvm.io/integration/zsh/
-__rvm_project_rvmrc
+for jr_rvm_path in $jr_rvm_paths
+do
+  # rvm exists at that path?
+  if [[ -s "$jr_rvm_path" ]] then
+    # Load RVM into a shell session *as a function*
+    source "$jr_rvm_path"
+    # As described on https://rvm.io/integration/zsh/
+    __rvm_project_rvmrc
+    # Don't check other paths (first found wins)
+    break
+  fi
+done
