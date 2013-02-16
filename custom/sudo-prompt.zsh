7f3e161f425449af3f0d1bca068751958af391e7
Jakob Rath <git@jakobrath.eu>
2013-02-16 17:00:40 +0100

    Check if sudo is installed before aliasing.

--- custom/sudo-prompt.zsh
+++ custom/sudo-prompt.zsh
@@ -1,1 +1,7 @@
-alias sudo='sudo -p "[sudo] enter password for %u: "'
+
+if type sudo >/dev/null 2>/dev/null; then
+
+  # sudo prompt should mention the user name
+  alias sudo='sudo -p "[sudo] enter password for %u: "'
+
+fi
