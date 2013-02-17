6af25152497d01331296676c53cd305c4efa6607
Jakob Rath <git@jakobrath.eu>
2013-02-17 10:40:02 +0100

    Change sudo alias to check for alias expansion. Now things like 'sudo ll' should work. See http://www.shellperson.net/using-sudo-with-an-alias/

--- custom/sudo-prompt.zsh
+++ custom/sudo-prompt.zsh
@@ -2,6 +2,6 @@
 if type sudo >/dev/null 2>/dev/null; then
 
   # sudo prompt should mention the user name
-  alias sudo='sudo -p "[sudo] enter password for %u: "'
+  alias sudo='sudo -p "[sudo] enter password for %u: " '
 
 fi
