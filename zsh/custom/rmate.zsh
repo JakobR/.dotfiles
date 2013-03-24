3ae8654891309e90e231da7890b7e29e70837d07
Jakob Rath <git@jakobrath.eu>
2013-03-24 15:09:33 +0100

    Add rmate as submodule.

--- zsh/custom/rmate.zsh
+++ zsh/custom/rmate.zsh
@@ -1,4 +1,2 @@
 
-# Check common rmate locations and alias if found
-[[ -s "/opt/rmate" ]]  && alias rmate="/opt/rmate"
-[[ -s "$HOME/rmate" ]] && alias rmate="$HOME/rmate"
+alias rmate="$HOME/.dotfiles/rmate/bin/rmate"
