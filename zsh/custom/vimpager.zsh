a756d52c4effcd26532f1995cf7aa633af596fd2
Jakob Rath <git@jakobrath.eu>
2013-04-18 00:29:10 +0200

    Fix vimpager detection.

--- zsh/custom/vimpager.zsh
+++ zsh/custom/vimpager.zsh
@@ -1,6 +1,12 @@
 
-# Use vimpager from submodule if not installed
-type vimpager >/dev/null 2>/dev/null || alias vimpager="$HOME/.dotfiles/vimpager/vimpager"
-type vimcat   >/dev/null 2>/dev/null || alias   vimcat="$HOME/.dotfiles/vimpager/vimcat"
-
+# Use vimpager instead of less
 export PAGER='vimpager'
+
+# Use vimpager from submodule if not installed
+type vimpager >/dev/null 2>/dev/null
+if [[ $? -ne 0 ]] then
+  alias vimpager="$HOME/.dotfiles/vimpager/vimpager"
+  export PAGER="$HOME/.dotfiles/vimpager/vimpager"
+fi
+
+type vimcat >/dev/null 2>/dev/null || alias vimcat="$HOME/.dotfiles/vimpager/vimcat"
