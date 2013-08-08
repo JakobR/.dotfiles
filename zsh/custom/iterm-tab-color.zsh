0fbe08028b3e6dd79b69acb912f301a957496a9d
Jakob Rath <git@jakobrath.eu>
2013-08-08 10:50:48 +0200

    Use yellow tab color for host 'kappa'.

--- zsh/custom/iterm-tab-color.zsh
+++ zsh/custom/iterm-tab-color.zsh
@@ -3,11 +3,14 @@
 # Only if not inside tmux and only when connecting with iTerm
 if [[ -z "$TMUX" && "$TERM_PROGRAM" == "iTerm.app" ]] then
   # Select desired color by hostname
-  if [[ `hostname` == "dango"* ]] then
+  HOSTNAME=$(hostname)
+  if [[ $HOSTNAME == "dango"* ]] then
     echo -n -e "\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;150\a\033]6;1;bg;blue;brightness;0\a"
-  elif  [[ `hostname` == "izumi"* ]] then
+  elif  [[ $HOSTNAME == "izumi"* ]] then
     echo -n -e "\033]6;1;bg;red;brightness;100\a\033]6;1;bg;green;brightness;100\a\033]6;1;bg;blue;brightness;255\a"
-  elif [[ `hostname` == "ichigo"* ]] then
+  elif [[ $HOSTNAME == "ichigo"* ]] then
     echo -n -e "\033]6;1;bg;red;brightness;190\a\033]6;1;bg;green;brightness;50\a\033]6;1;bg;blue;brightness;50\a"
+  elif [[ $HOSTNAME == "kappa"* ]] then
+    echo -n -e "\033]6;1;bg;red;brightness;230\a\033]6;1;bg;green;brightness;210\a\033]6;1;bg;blue;brightness;0\a"
   fi
 fi
