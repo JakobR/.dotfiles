2e8b3ab1256d27a7b8fa5d9319abfc7a29ecfcab
Jakob Rath <git@jakobrath.eu>
2014-01-31 16:07:52 +0100

    Add rvm to PATH.

--- zsh/custom/rvm.zsh
+++ zsh/custom/rvm.zsh
@@ -1,13 +1,14 @@
-
 # Possible rvm install paths
-jr_rvm_paths=("$HOME/.rvm/scripts/rvm" "/usr/local/rvm/scripts/rvm")
+jr_rvm_paths=("$HOME/.rvm" "/usr/local/rvm")
 
 for jr_rvm_path in $jr_rvm_paths
 do
   # rvm exists at that path?
-  if [[ -s "$jr_rvm_path" ]] then
+  if [[ -s "$jr_rvm_path/scripts/rvm" ]] then
+    # Add RVM to PATH for scripting
+    PATH="$PATH:$jr_rvm_path/bin"
     # Load RVM into a shell session *as a function*
-    source "$jr_rvm_path"
+    source "$jr_rvm_path/scripts/rvm"
     # As described on https://rvm.io/integration/zsh/
     __rvm_project_rvmrc
     # Don't check other paths (first found wins)
