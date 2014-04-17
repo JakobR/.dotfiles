fc380fc73b6735ae30b4bcc8937dc2c53d379829
Jakob Rath <git@jakobrath.eu>
2014-04-17 08:45:29 +0200

    Update dwim transformations for `dtr`.

--- zsh/custom/dwim.zsh
+++ zsh/custom/dwim.zsh
@@ -4,8 +4,9 @@ bindkey "^U" backward-kill-line
 
 #    dtr
 # => dtr --list
-_dwim_add_transform '^dtr *(.*--remove-and-delete)?$' \
-  'BUFFER="dtr --list"'
+_dwim_add_transform '^dtr *(.*--)$' \
+  'BUFFER="dtr --list"
+  _dwim_cursor=$#BUFFER'
 
 #    dtr --list
 # => dtr -t
@@ -14,14 +15,20 @@ _dwim_add_transform '^dtr --list *$' \
   _dwim_cursor=$#BUFFER'
 
 #    dtr -t 1,2,3
-# => dtr -t 1,2,3 --info | grep Name
+# => dtr -t 1,2,3 --list
 _dwim_add_transform '^dtr -t [0-9,-]+ *(--remove-and-delete)?$' \
-  '_dwim_sed "s/ *(--remove-and-delete)?$/ --info | grep Name/"'
+  '_dwim_sed "s/ *(--remove-and-delete)?$/ --list/"'
 
-#    dtr -t 1,2,3 --info | grep Name
+#    dtr -t 1,2,3 --list
 # => dtr -t 1,2,3 --remove-and-delete
-_dwim_add_transform '^dtr -t [0-9,-]+ *--info *\| *grep N?ame' \
-  '_dwim_sed "s/--info.*/--remove-and-delete/"'
+_dwim_add_transform '^dtr -t [0-9,-]+ *--list' \
+  '_dwim_sed "s/--list.*/--remove-and-delete/"'
+
+#    dtr -t 1,2,3 --remove-and-delete
+# => dtr -t 1,2,3 --
+_dwim_add_transform '^dtr -t [0-9,-]+ *--remove-and-delete$' \
+  '_dwim_sed "s/ *--remove-and-delete$/ --/"
+  _dwim_cursor=$#BUFFER'
 
 
 #     brew info <formula>
