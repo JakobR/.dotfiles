e86c77f837c63f235fdc00f879c5ad14d32294c9
Jakob Rath <mail@jakobrath.eu>
2014-10-03 20:14:47 +0200

    more slate hotkeys

--- slate.js
+++ slate.js
@@ -58,6 +58,8 @@ slate.bind(hyper("s"), focus("Google Chrome"));
 slate.bind(hyper("f"), focus("Finder"));
 slate.bind(hyper("j"), focus("JEDict"));
 slate.bind(hyper("x"), focus("Xcode"));
+slate.bind(hyper("y"), focus("YNAB 4"));
+slate.bind(hyper("w"), focus("Things"));
 
 //slate.config("windowHintsIgnoreHiddenWindows", false);
 //slate.config("windowHintsShowIcons", true);
