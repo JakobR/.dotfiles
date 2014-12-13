f0b8c60048f454c36535058768b9f229ac2a785f
Jakob Rath <mail@jakobrath.eu>
2014-12-13 13:44:26 +0100

    Load rvm only on demand (reduces zsh startup time by about 0.20s).

--- zsh/custom/rvm.zsh
+++ zsh/custom/rvm.zsh
@@ -1,17 +1,27 @@
-# Possible rvm install paths
-jr_rvm_paths=("$HOME/.rvm" "/usr/local/rvm")
+rvm () {
+  unset -f rvm
+  echo "Loading rvm..."
 
-for jr_rvm_path in $jr_rvm_paths
-do
-  # rvm exists at that path?
-  if [[ -s "$jr_rvm_path/scripts/rvm" ]] then
-    # Add RVM to PATH for scripting
-    PATH="$PATH:$jr_rvm_path/bin"
-    # Load RVM into a shell session *as a function*
-    source "$jr_rvm_path/scripts/rvm"
-    # As described on https://rvm.io/integration/zsh/
-    __rvm_project_rvmrc
-    # Don't check other paths (first found wins)
-    break
+  # Possible rvm install paths
+  jr_rvm_paths=("$HOME/.rvm" "/usr/local/rvm")
+
+  for jr_rvm_path in $jr_rvm_paths
+  do
+    # rvm exists at that path?
+    if [[ -s "$jr_rvm_path/scripts/rvm" ]] then
+      # Add RVM to PATH for scripting
+      PATH="$PATH:$jr_rvm_path/bin"
+      # Load RVM into a shell session *as a function*
+      source "$jr_rvm_path/scripts/rvm"
+      # As described on https://rvm.io/integration/zsh/
+      __rvm_project_rvmrc
+      # Don't check other paths (first found wins)
+      break
+    fi
+  done
+
+  # If we got arguments, pass them to rvm
+  if (( $# > 0 )) then
+    rvm "$@"
   fi
-done
+}
