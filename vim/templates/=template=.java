a5ba735a417e076bc4fbe323e999df33f7374888
Jakob Rath <mail@jakobrath.eu>
2015-10-13 09:52:17 +0200

    Change java template

--- vim/templates/=template=.java
+++ vim/templates/=template=.java
@@ -1,13 +1,7 @@
-/*
- * %FFILE%
- * Copyright (C) %YEAR% %USER% <%MAIL%>
- *
- * Distributed under terms of the %LICENSE% license.
- */
+package %HERE%;
 
 public class %FILE%
 {
     public %FILE%() {
-        %HERE%
     }
 }
