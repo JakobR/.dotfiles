bc9840639fa60dcf82885bdb522cea3871795f04
Jakob Rath <mail@jakobrath.eu>
2015-12-04 15:24:58 +0100

    Update config for tmux 2.1

--- zsh/custom/tmux.zsh
+++ zsh/custom/tmux.zsh
@@ -7,7 +7,10 @@ if type tmux >/dev/null 2>/dev/null; then
     jr_tmux_sessions=`tmux list-sessions 2>&1`
 
     # Are there any tmux sessions?
-    if [[ "$jr_tmux_sessions" != "failed to connect to server"* ]] then
+    if [[ ( "$jr_tmux_sessions" != "failed to connect to server"* ) &&
+          ( "$jr_tmux_sessions" != "no server running on"* ) &&
+          ( "$jr_tmux_sessions" != "error connecting to"* )
+       ]] then
 
       # If yes, list them
       echo "\n\033[4mActive tmux sessions:\033[0m\n$jr_tmux_sessions\n"
