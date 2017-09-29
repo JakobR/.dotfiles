f74d5cfaa37caaf6d6587f6e15d584bdcdff7c58
Jakob Rath <git@jakobrath.eu>
2017-09-29 18:45:23 +0200

    remove package list from custom.el

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (flycheck-pos-tip flycheck-color-mode-line evil-org solarized-theme quelpa-use-package nlinum-relative intero helm-projectile flycheck-haskell evil-surround evil-numbers evil-nerd-commenter evil-leader evil-indent-textobject emacs-color-theme-solarized))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
