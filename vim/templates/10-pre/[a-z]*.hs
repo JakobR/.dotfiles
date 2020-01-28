fd6adcedc6f3b90e0385615d6e44991af1296e91
Jakob Rath <git@jakobrath.eu>
2020-01-28 12:30:03 +0100

    Update template for standalone Haskell scripts

--- vim/templates/10-pre/[a-z]*.hs
+++ vim/templates/10-pre/[a-z]*.hs
@@ -1,10 +1,29 @@
 #!/usr/bin/env stack
 {- stack script
   --resolver %HASKELLRESOLVER%
+  --package rio
 -}
 
+{-# OPTIONS_GHC -Wall #-}
+{-# OPTIONS_GHC -Wcompat #-}
+{-# OPTIONS_GHC -Widentities #-}
+{-# OPTIONS_GHC -Wincomplete-record-updates #-}
+{-# OPTIONS_GHC -Wincomplete-uni-patterns #-}
+{-# OPTIONS_GHC -Wpartial-fields #-}
+{-# OPTIONS_GHC -Wredundant-constraints #-}
+{-# OPTIONS_GHC -Wnoncanonical-monad-instances #-}
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
-%HERE%
+-- rio
+import RIO
+import qualified RIO.Text as T
+
+
+main :: IO ()
+main = do
+  %HERE%putStrLn "Hi"
