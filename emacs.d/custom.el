e649b393d5c6e4483cc0ec3700322376eda57124
Jakob Rath <git@jakobrath.eu>
2020-09-21 12:19:53 +0200

    Customize faces in Emacs

--- emacs.d/custom.el
+++ emacs.d/custom.el
@@ -9,4 +9,14 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t (:family "Menlo" :height 1.0 :width normal :weight normal))))
+ '(org-document-title ((t (:weight bold :height 1.2))))
+ '(org-level-1 ((t (:weight bold :height 1.1))))
+ '(org-level-2 ((t (:weight bold :height 1.05))))
+ '(org-level-3 ((t (:weight bold :height 1.0))))
+ '(org-level-4 ((t (:weight bold :height 1.0))))
+ '(org-level-5 ((t (:weight bold))))
+ '(org-level-6 ((t (:weight bold))))
+ '(org-level-7 ((t (:weight bold))))
+ '(org-level-8 ((t (:weight bold))))
+ '(variable-pitch ((t (:family "Source Sans Pro" :height 180 :width normal :weight normal)))))
