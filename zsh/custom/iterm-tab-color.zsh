3c9b6128b4c59d809c409b3b0de63ba2b6d5709c
Jakob Rath <git@jakobrath.eu>
2020-10-05 18:54:06 +0200

    Update iTerm tab colors

--- zsh/custom/iterm-tab-color.zsh
+++ zsh/custom/iterm-tab-color.zsh
@@ -6,10 +6,10 @@ if [[ -z "$TMUX" && "$TERM_PROGRAM" == "iTerm.app" ]] then
   HOSTNAME=$(hostname)
   if [[ $HOSTNAME == "dango"* ]] then
     echo -n -e "\033]6;1;bg;red;brightness;0\a\033]6;1;bg;green;brightness;150\a\033]6;1;bg;blue;brightness;0\a"
+  elif [[ $HOSTNAME == "yotsuba"* ]] then
+    echo -n -e "\033]6;1;bg;red;brightness;190\a\033]6;1;bg;green;brightness;50\a\033]6;1;bg;blue;brightness;50\a"
   elif  [[ $HOSTNAME == "izumi"* ]] then
     echo -n -e "\033]6;1;bg;red;brightness;100\a\033]6;1;bg;green;brightness;100\a\033]6;1;bg;blue;brightness;255\a"
-  elif [[ $HOSTNAME == "ichigo"* ]] then
-    echo -n -e "\033]6;1;bg;red;brightness;190\a\033]6;1;bg;green;brightness;50\a\033]6;1;bg;blue;brightness;50\a"
   elif [[ $HOSTNAME == "kappa"* ]] then
     echo -n -e "\033]6;1;bg;red;brightness;230\a\033]6;1;bg;green;brightness;210\a\033]6;1;bg;blue;brightness;0\a"
   fi
