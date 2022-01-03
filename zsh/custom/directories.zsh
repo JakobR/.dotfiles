074d58c5871b9ce04d35891c9926ceb6a71ca46f
Jakob Rath <git@jakobrath.eu>
2022-01-03 16:30:59 +0100

    Merge files

--- zsh/custom/directories.zsh
+++ zsh/custom/directories.zsh
@@ -27,3 +27,17 @@ function d () {
   fi
 }
 compdef _dirs d
+
+if [[ ( "$OSTYPE" =~ ^linux ) || ( -n "${IN_NIX_SHELL:-}") ]] then
+    # Aliases for GNU coreutils (they are used in nix-shell even on macOS)
+    alias ls="ls -F --color=auto"
+    alias ll="ls -lhF --time-style=long-iso"
+elif [[ ($OSTYPE =~ ^darwin) || ($OSTYPE =~ ^freebsd) ]] then
+    alias ls="ls -FG"
+    alias ll="ls -lhF -D '%F %T'"
+else
+    alias ls='ls -F'
+    alias ll='ls -lhF'
+fi
+
+alias la="ll -a"
