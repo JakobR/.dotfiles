a90db9536ce1e7a3bb21c50fa93673caf0cf3f38
Jakob Rath <git@jakobrath.eu>
2022-01-03 23:07:14 +0100

    Need magic-space to update highlighting while typing

--- zsh/custom/key-bindings.zsh
+++ zsh/custom/key-bindings.zsh
@@ -1,3 +1,5 @@
 # TODO: maybe take some bindings from https://github.com/ohmyzsh/ohmyzsh/blob/master/lib/key-bindings.zsh
 bindkey '^U' backward-kill-line
 bindkey '^X^_' redo
+
+bindkey ' ' magic-space
