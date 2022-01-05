ce55ac147b9b94aeab7d6425d5215faa54f41f47
Jakob Rath <git@jakobrath.eu>
2022-01-05 22:26:16 +0100

    minor

--- zsh/conf/ag.zsh
+++ zsh/conf/ag.zsh
@@ -1,10 +1,15 @@
 if type ag >/dev/null 2>/dev/null; then
 
-  # Usually I want to search case-insensitively.
-  # Use \ag (prefixed with backslash) for case sensitive search.
-  alias ag='ag --ignore-case'
+    # Usually I want to search case-insensitively.
+    # Use \ag (prefixed with backslash) for case sensitive search.
+    alias ag='ag --ignore-case'
 
 fi
 
 # alias grep="grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn}"
 alias grep="grep --color=auto"
+
+# Some linux distributions install 'fd' as 'fdfind'
+if (( $+commands[fdfind] )) then
+    alias fd=fdfind
+fi
