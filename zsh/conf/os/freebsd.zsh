8977d884fb6fa46c468314967ee268a07d0ef605
Jakob Rath <git@jakobrath.eu>
2022-06-24 05:41:42 +0200

    Set TERMCAP on FreeBSD

--- zsh/conf/os/freebsd.zsh
+++ zsh/conf/os/freebsd.zsh
@@ -4,3 +4,6 @@
 
 # Show full command name in top by default (-a)
 export TOP='-a'
+
+# Add the capability to "clear the screen" after exiting vi, more/less, etc.
+export TERMCAP='xterm-256color:te=\E[?1049l:ti=\E[?1049h:tc=xterm-256color'
